5ea796a00cd9d07755ca61cf244bae66b7e31aff
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2026-05-20 18:40:08 -0300

    nueva carpeta y, creacion y uso de database

--- app_backend/db_scripts/init_db.sql
+++ app_backend/db_scripts/init_db.sql
@@ -1,12 +1,17 @@
+CREATE DATABASE IF NOT EXISTS tp_integrador_ids;
+USE tp_integrador_ids;
+
 CREATE TABLE IF NOT EXISTS usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     mail VARCHAR(50) UNIQUE NOT NULL,
-    score INT DEFAULT 0
+    score INT DEFAULT 0,
+    rol VARCHAR(20) DEFAULT 'alumno',
+    carrera VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS articulos (
-    id_articulo INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nombre_art VARCHAR(50) NOT NULL,
     tipo VARCHAR(50) NOT NULL,
     seccion VARCHAR(50) NOT NULL,
@@ -16,29 +21,23 @@ CREATE TABLE IF NOT EXISTS articulos (
 );
 
 CREATE TABLE IF NOT EXISTS reserva (
-    id_reserva INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     id_usuario INT NOT NULL,
     id_reservado INT NOT NULL,
-    estado_reserva VARCHAR(20) NOT NULL DEFAULT pendiente,
+    estado_reserva VARCHAR(20) NOT NULL DEFAULT 'pendiente',
     fecha_retiro DATETIME NOT NULL,
     fecha_regreso DATETIME NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id),
-    FOREIGN KEY (id_reservado) REFERENCES articulos(id_articulo)
+    FOREIGN KEY (id_reservado) REFERENCES articulos(id)
 );
 
 CREATE TABLE IF NOT EXISTS estado_devuelto (
-    id_devuelto INT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_reserva INT NOT NULL,
     dias_retraso INT DEFAULT 0,
     condiciones VARCHAR(50),
-    FOREIGN KEY (id_devuelto) REFERENCES reserva(id_reserva)
+    FOREIGN KEY (id_reserva) REFERENCES reserva(id)
 );
-
-
-ALTER TABLE usuario 
-ADD rol VARCHAR(20) DEFAULT 'alumno';
-
-ALTER TABLE usuario 
-ADD carrera VARCHAR(50);
 
 CREATE TABLE IF NOT EXISTS penalizacion (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -56,7 +55,7 @@ CREATE TABLE IF NOT EXISTS qr (
     fecha_generado DATETIME NOT NULL,
     codigo VARCHAR(255) UNIQUE,
     escaneado BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva)
+    FOREIGN KEY (id_reserva) REFERENCES reserva(id)
 );
 
 CREATE TABLE IF NOT EXISTS normativa (
